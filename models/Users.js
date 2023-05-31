ce95b01e7d355b167fd923968c3caeb7c7a2edbb
AngelMond <abraham_will@hotmail.com>
2023-05-31 01:32:18 -0500

    Created db models

---
+++ models/Users.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//Users Schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+    registerDate: {
+        type: Date,
+        default: Date.now,
+        get: (date) => date.toLocaleDateString(),
+    },
+},
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+//Before saved, hashed password
+schema.pre('save', async function (next) {
+    try {
+        if (this.isModified('password')) {
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashedPassword;
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+//Compare password
+schema.methods.comparePassword = async function (password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const User = model('Users', userSchema);
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Users = require('./Users');
+const UsersCheck = require('./UsersCheck');
+
+module.exports = {
+    Users,
+    UsersCheck,
+}
