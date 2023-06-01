3ab8a4a92c341f0f44da90c05019f98fd5e8b89c
AngelMond <abraham_will@hotmail.com>
2023-06-01 01:18:53 -0600

    changed userRoutes controller comments

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 
 const userController = require('../../controllers/userController');
 
-//User login routes
-router.post('/signup', userController.createUser); //create controller
-router.post('/login', userController.getUser); //get user controller
+//User routes
+router.post('/signup', userController.createUser); //signup controller
+router.post('/login', userController.getUser); //login controller
 
 
 
