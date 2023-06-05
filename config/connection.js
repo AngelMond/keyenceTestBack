c623f5e9487800383b4b286a9826fd39576433b7
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-04 23:43:59 -0600

    updated

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
 // const mongoURI = 'mongodb://127.0.0.1:27017/keyence';
-const mongoURI = process.env.MONGO_URI;
+// const mongoURI = process.env.MONGO_URI;
+const mongoURI = 'mongodb+srv://angelMC:dbMongo182!!@cluster0.gkk2awe.mongodb.net';
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
