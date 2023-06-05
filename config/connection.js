6751038e82aae3aff2f42cb7f4f6edd5e1b004fe
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-05 00:09:27 -0600

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
 // const mongoURI = 'mongodb://127.0.0.1:27017/keyence';
 // const mongoURI = process.env.MONGO_URI;
-const mongoURI = 'mongodb+srv://angelMC:dbMongo182!!@cluster0.gkk2awe.mongodb.net';
+// const mongoURI = 'mongodb+srv://angelMC:dbMongo182!!@cluster0.gkk2awe.mongodb.net';
+const mongoURI = 'mongodb+srv://angelMC:dbMongo182!!@cluster0.gkk2awe.mongodb.net/?retryWrites=true&w=majority';
+
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
