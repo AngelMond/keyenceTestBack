2a599aca28490a73a763acaff8c010af65e05f00
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-04 23:34:28 -0600

    commented cors on main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 // const session = require('express-session');
-const cors = require('cors');
+// const cors = require('cors');
 
 
 
