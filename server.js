5917fe7f7d2dea49a1b74a85982c5d5417762e40
AngelMondragon <angel.mondragon@4n.com.mx>
2023-06-04 21:46:44 -0600

    updated server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = require('./config/connection');
 //Import Routes
 const routes = require('./routes')
 
-const PORT = process.env.PORT;
+const PORT = 3001;
 const app = express();
 
 // Set up sessions
