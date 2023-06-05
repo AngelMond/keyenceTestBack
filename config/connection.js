36fa978fe9a1c5e8268f390b33c1f4688ee28301
AngelMond <abraham_will@hotmail.com>
2023-06-04 19:53:42 -0600

    set mongoURI to connect to atlas db

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 'use strict'
+require('dotenv').config();
 const mongoose = require('mongoose');
 //mongoose.Promise = global.Promise;
-const mongoURI = 'mongodb://127.0.0.1:27017/keyence';
+// const mongoURI = 'mongodb://127.0.0.1:27017/keyence';
+const mongoURI = process.env.MONGO_URI;
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
